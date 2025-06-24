773d4a8e25d7658b926ba5957c51dcad1307ab4d
Leonardo8896 <pkafonso8896@gmail.com>
2025-06-24 19:28:17 -0300

    Adição de rota para teste de servidor

---
+++ api/src/Controllers/TestServerController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Leonardo8896\Hydrignis\Controllers;
+
+class TestServerController
+{
+    public static function index(): void
+    {
+        echo "Server is running!";
+    }
+}
