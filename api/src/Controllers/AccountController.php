23da53f75387040c0c7a0f6c9699494c6e085fd2
Leonardo8896 <pkafonso8896@gmail.com>
2025-06-24 19:27:44 -0300

    Arquivos de base para sistema de contas

---
+++ api/src/Controllers/AccountController.php
@@ -0,0 +1,15 @@
+<?php
+namespace Leonardo8896\Hydrignis\Controllers;
+
+class AccountController
+{
+    public static function login(): void
+    {
+
+    }
+
+    public static function register(): void
+    {
+        
+    }
+}
--- api/src/Exceptions/FieldException.php
+++ api/src/Exceptions/FieldException.php
@@ -3,3 +3,6 @@
 namespace Leonardo8896\Hydrignis\Exceptions;
 
 class FieldException extends \DomainException
+{
+    
+}
