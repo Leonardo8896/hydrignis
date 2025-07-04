779969252b98ef5e1d140b9f1882f4e4cff7b92f
Leonardo8896 <pkafonso8896@gmail.com>
2025-07-04 01:01:33 -0300

    adição de classe para tratar playload vindo do IgnisZero

---
+++ websocket/src/Model/IgnisPlayload.php
@@ -0,0 +1,67 @@
+<?php
+namespace Hydrignis\Websocket\Model;
+
+class IgnisPlayload {
+    private array $image_array;
+    private float $gas;
+    private int $status;
+    private int $onoff;
+    public function __construct($data) {
+        $this->dataLoad($data);
+    }
+
+    private function dataLoad($bin): void
+    {
+        if ($bin[0] !== "\xAA" || $bin[1] !== "\x55") {
+            return;
+        }
+
+        $playload = substr($bin, 2);
+
+        $data = explode("\xAA\x55", $playload);
+        $this->image_array = $this->bin8ToArrayFloat($data[0], 768);
+        $this->gas = $this->bin8ToFloat($data[1]);
+        $this->status = $this->bin8ToInt($data[2]);
+        $this->onoff = $this->bin8ToInt($data[3]);
+    }
+    private function bin8ToArrayFloat($bin, $len): array|null
+    {
+        // if ($bin[0] !== "\xAA" || $bin[1] !== "\x55") {
+        //     return null;
+        // }
+
+        // $playload = substr($bin, 2);
+
+        if (strlen($bin) !== $len*4) {
+            return null;
+        }
+
+        $frame = unpack('g*', $bin);
+
+        return $frame;
+    }
+
+    private function bin8ToFloat($bin): float
+    {
+        $frame = unpack('g', $bin);
+        return $frame;;
+    }
+
+    private function bin8ToInt($bin): int
+    {
+        $frame = unpack('C', $bin);
+        return $frame;
+    }
+
+    public function getIgnislog(): array
+    {
+        $playload = [
+            "Camera" => $this->image_array,
+            "Gas" => $this->gas,
+            "Status" => $this->status,
+            "Onoff" => $this->onoff
+        ];
+
+        return $playload;
+    }
+}
