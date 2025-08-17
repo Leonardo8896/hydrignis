285b39a1e4a9dfdae9c659ee2d1d50f1b090a643
Leonardo8896 <pkafonso8896@gmail.com>
2025-08-17 18:01:01 -0300

    fix & refactor: correção do searchByEmail e adição do método save no UserRepository

--- api/src/Database/Repository/UserRepository.php
+++ api/src/Database/Repository/UserRepository.php
@@ -13,15 +13,29 @@ class UserRepository
 
     public function searchByEmail(string $email): User|null
     {
-        $query = $this->pdo->prepare("SELECT * FROM USERS WHERE rm= :rm");
-        $query->bindParam(":rm", $email);
+        $query = $this->pdo->prepare("SELECT * FROM USERS WHERE email= :email");
+        $query->bindParam(":email", $email);
         $query->execute();
-
+        if ($query->rowCount() === 0) {
+            return null;
+        }
         return $this->hydrateUser($query->fetch(\PDO::FETCH_ASSOC));
     }
 
     private function hydrateUser(array $user): User
     {
         return new User($user['email'], $user['name'], $user['password']);
+    }
+
+    public function save(User $user): void
+    {
+        $query = $this->pdo->prepare("INSERT INTO USERS (email, name, password) VALUES (:email, :name, :password)");
+        $email = $user->email;
+        $query->bindParam(":email", $email);
+        $name = $user->name;
+        $query->bindParam(":name", $name);
+        $password = $user->getPassword();
+        $query->bindParam(":password", $password);
+        $query->execute();
     }
 }
