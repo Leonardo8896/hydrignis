876c0dd5ab10aca827b83a9824823cc6d2570ffb
Leonardo8896 <pkafonso8896@gmail.com>
2025-08-17 17:59:16 -0300

    fix: correção de nomes de variáveis de ambiente e de rota do CA

--- api/src/Database/Core/ConnectionCreator.php
+++ api/src/Database/Core/ConnectionCreator.php
@@ -8,12 +8,12 @@ class ConnectionCreator
 {
     public static function createPDOConnection(): PDO
     {
-        $connectionString = "mysql:host={$_ENV['HOST']};port={$_ENV['PORT']};dbname={$_ENV['DB_NAME']}";
+        $connectionString = "mysql:host={$_ENV['DB_HOST']};port={$_ENV['DB_PORT']};dbname={$_ENV['DB_NAME']}";
 
         $connection = new PDO(
             $connectionString,
-            $_ENV['USER'],
-            $_ENV['PASSWORD'],
+            $_ENV['DB_USER'],
+            $_ENV['DB_PASSWORD'],
             [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -29,9 +29,9 @@ class ConnectionCreator
 
     private static function createTempCaPath():string
     {
-        $certificado = str_replace('\\n', "\n", $_ENV['CA_FILE']);
+        $certificado = str_replace('\\n', "\n", $_ENV['DB_CA_FILE']);
 
-        $tempPath = sys_get_temp_dir()."isrgrootx1.pem";
+        $tempPath = sys_get_temp_dir().DIRECTORY_SEPARATOR."isrgrootx1.pem";
         file_put_contents($tempPath, $certificado);
 
         return $tempPath;
