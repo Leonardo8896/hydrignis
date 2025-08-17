273bae0ef2e47943f6f182e14a09e4bdd649dd3c
Leonardo8896 <pkafonso8896@gmail.com>
2025-08-17 15:36:22 -0300

    refactor: add of an register method and a saveCredential to make and send JWT tokens

--- api/src/Service/AccountService.php
+++ api/src/Service/AccountService.php
@@ -2,27 +2,60 @@
 
 namespace Leonardo8896\Hydrignis\Service;
 
-use Leonardo8896\Hydrignis\Database\Repository\Interface\UserRepository;
+use Leonardo8896\Hydrignis\Database\Repository\UserRepository;
 use Leonardo8896\Hydrignis\Exceptions\AccountException;
+use Leonardo8896\Hydrignis\Model\User;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Leonardo8896\Hydrignis\Exceptions\FieldException;
 
 class AccountService
 {
+    private User $user;
     public function __construct(
         private UserRepository $userRepository,
         private string $email,
-        private ?string $name,
+        private ?string $name = null,
     )
     {}
 
-    public function login(string $email, string $password): void
+    public function login(string $password): string
     {
-        $user = $this->userRepository->searchByEmail($email);
-        if (!$user) {
-            throw new AccountException();
+        $this->user = $this->userRepository->searchByEmail($this->email);
+        if (!$this->user) {
+            throw new AccountException("Conta inexistente ou senha incorreta.");
         }
 
-        if(!$user->verifyPassword($password)) {
-            throw new AccountException();
+        if(!$this->user->verifyPassword($password)) {
+            throw new AccountException("Conta inexistente ou senha incorreta.");
         }
+
+        return $this->saveCredentials();
+    }
+
+    private function saveCredentials(): string
+    {
+        $playload = [
+            "iss" => $_ENV["TOKENS_EMISSOR"],
+            "iat" => time(),
+            "exp" => time() + $_ENV["TOKENS_EXP"],
+            "user_email" => $this->email
+        ];
+
+        return JWT::encode($playload, $_ENV["TOKENS_API"], 'HS256');
+    }
+
+    public function register(string $password): string
+    {
+        if ($this->userRepository->searchByEmail($this->email)) {
+            throw new AccountException("Já existe uma conta com esse email");
+        }
+
+        if (!User::checkStrength($password)) {
+            throw new FieldException();
+        }
+
+        $this->user = new User($this->email, $this->name, User::hashPassword($password));
+        return $this->saveCredentials();
     }
 }
