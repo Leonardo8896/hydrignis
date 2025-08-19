8530ba181a5a01403e8a438bba7b5bc1487a12e8
Leonardo8896 <pkafonso8896@gmail.com>
2025-08-18 21:51:04 -0300

    add: criação das classes referentes aos relatórios de incêndio do Ignis

---
+++ api/src/Database/Repository/FireAccidentRepository.php
@@ -0,0 +1,35 @@
+<?php
+namespace Leonardo8896\Hydrignis\Database\Repository;
+
+use Leonardo8896\Hydrignis\Model\FireAccident;
+
+class FireAccidentRepository
+{
+    // Assuming you have a PDO instance for database operations
+    private $pdo;
+
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    // Example method to fetch fire accidents by device serial number
+    public function getFireAccidentsByDeviceSerial(string $serialNumber): array
+    {
+        $query = $this->pdo->prepare("SELECT * FROM FIRE_ACCIDENTS WHERE device_serial_number = :serial_number");
+        $query->bindParam(":serial_number", $serialNumber);
+        $query->execute();
+
+        $results = $query->fetchAll(\PDO::FETCH_ASSOC);
+        return array_map([$this, 'hydrateFireAccident'], $results);
+    }
+
+    private function hydrateFireAccident(array $data): FireAccident
+    {
+        return new FireAccident(
+            id: $data['id'],
+            date: $data['date'],
+            time: $data['time']
+        );
+    }
+}
--- api/src/Model/FireAccident.php
+++ api/src/Model/FireAccident.php
@@ -9,4 +9,13 @@ class FireAccident
         public readonly string $date,
         public readonly string $time,
     ){}
+
+    public function __serialize(): array
+    {
+        return [
+            'id' => $this->id,
+            'date' => $this->date,
+            'time' => $this->time,
+        ];
+    }
 }
