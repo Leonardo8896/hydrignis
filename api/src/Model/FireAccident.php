e576d176f3acdb727d537c3d56165617fcbdb070
leonardo8896 <pkafonso8896@gmail.com>
2025-09-02 09:44:41 -0300

    refactor: update FireAccident model to use readonly properties and improve serialization

--- api/src/Model/FireAccident.php
+++ api/src/Model/FireAccident.php
@@ -5,17 +5,27 @@ namespace Leonardo8896\Hydrignis\Model;
 class FireAccident
 {
     public function __construct(
-        public readonly string $id,
         public readonly string $date,
         public readonly string $time,
+        public readonly string $serialNumber,
+        private ?string $id = null,
     ){}
 
-    public function __serialize(): array
+    public function __get(string $name): ?string
+    {
+        if ($name === "id") {
+            return $this->id;
+        }
+        return null;
+    }
+
+    public function toArray(): array
     {
         return [
             'id' => $this->id,
             'date' => $this->date,
             'time' => $this->time,
+            'serial_number'=> $this->serialNumber,
         ];
     }
 }
