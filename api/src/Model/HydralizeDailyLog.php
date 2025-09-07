81da925aa810450fa3ced4149373634b401e40e8
leonardo8896 <pkafonso8896@gmail.com>
2025-09-07 20:44:44 -0300

    refactor: update HydralizeDailyLog and IgnisZeroDailyLog constructors and toArray methods for improved property handling

--- api/src/Model/HydralizeDailyLog.php
+++ api/src/Model/HydralizeDailyLog.php
@@ -5,10 +5,23 @@ namespace Leonardo8896\Hydrignis\Model;
 class HydralizeDailyLog
 {
     public function __construct(
-        public readonly int $id,
+        public readonly int $serialNumber,
         public readonly string $date,
         public readonly float $water_production,
         public readonly int $energy_consumption,
         public readonly int $battery_consumption,
+        private ?int $id = null,
     ){}
+
+    public function toArray(): array
+    {
+        return [
+            "id" => $this->id,
+            "serial_number" => $this->serialNumber,
+            "date" => $this->date,
+            "water_production" => $this->water_production,
+            "energy_consumption" => $this->energy_consumption,
+            "battery_consumption" => $this->battery_consumption,
+        ];
+    }
 }
--- api/src/Model/IgnisZeroDailyLog.php
+++ api/src/Model/IgnisZeroDailyLog.php
@@ -5,9 +5,20 @@ namespace Leonardo8896\Hydrignis\Model;
 class IgnisZeroDailyLog
 {
     public function __construct(
-        public readonly string $id,
+        public readonly int $serialNumber,
         public readonly string $date,
         public readonly string $energyConsumption,
+        private ?int $id = null,
     )
     {}
+
+    public function toArray(): array
+    {
+        return [
+            "id" => $this->id,
+            "serial_number" => $this->serialNumber,
+            "date" => $this->date,
+            "energy_consumption" => $this->energyConsumption,
+        ];
+    }
 }
